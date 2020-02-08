e2828a722f9bedf5d4c4bdc6656af9a840b50eab
hpnok <pottagebleu@hotmail.com>
2020-02-07 21:38:44 -0500

    keep reference

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ try:
     from pytmx.util_pygame import load_pygame
 except ModuleNotFoundError:
     print('Warning: Pytmx is not installed')
-    
+
 import traceback
 from math import sin, cos, pi
 from os import path
@@ -32,7 +32,7 @@ def load_map(folder, name):
             layer_data.append(layer.data)
             for x, y, image in layer.tiles():
                 if image:
-                    bg_image.blit(image, (x * tiled_map.tilewidth, 
+                    bg_image.blit(image, (x * tiled_map.tilewidth,
                                           y * tiled_map.tileheight))
     return bg_image, layer_data
 
@@ -50,91 +50,91 @@ class Game:
         self.fps = 30
         self.all_sprites = pg.sprite.Group()
         self.running = True
-        
+
         # specify the directories for asset loading
         base_dir = path.dirname(__file__)
         assets_folder = path.join(base_dir, 'assets')
-        
+
         player_image_strip = pg.image.load(
                 path.join(assets_folder, 'kart.png')).convert_alpha()
-        self.player_images = [player_image_strip.subsurface((i * 30, 0, 30, 32)) 
+        self.player_images = [player_image_strip.subsurface((i * 30, 0, 30, 32))
                               for i in range(11)]
-        
+
         self.cloud_image = pg.image.load(
                 path.join(assets_folder, 'cloud.png')).convert_alpha()
         self.traffic_light_images = pg.image.load(
                 path.join(assets_folder, 'lights.png')).convert_alpha()
         self.bush_image = pg.image.load(
                 path.join(assets_folder, 'bush.png')).convert_alpha()  # TODO: unused
-        
+
         self.player = Player(self)
         self.traffic_light = TrafficLight(self, (100, 60))
-        
+
         try:
             self.map_img, _ = load_map(folder=assets_folder, name='track2')
             self.map = Mode7(self, self.map_img)
         except Exception:
             traceback.print_exc()
             self.map = Mode7(self)
-            
+
         self.started = False
-        
-    
+
+
     def events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.running = False
 
-    
+
     def update(self, dt):
         self.game_screen.blit(self.background, (0, 0))
         self.map.update(dt)
         self.all_sprites.update(dt)
-        
+
         if self.traffic_light.done:
             self.started = True
-        
+
 
     def draw(self):
         for s in self.all_sprites:
             s.draw(self.game_screen)
-        
+
 # =============================================================================
 #         #for debugging
 #         start1 = vec(self.player.rect.center)
 #         end1 = start1 + self.player.vel * 300
 #         pg.draw.line(self.fake_screen, pg.Color('white'), start1, end1)
-#         
+#
 #         start2 = vec(self.player.rect.center)
 #         v = vec()
 #         v.from_polar((20, self.player.angle * 180 / pi))
 #         end2 = start2 + v
 #         pg.draw.line(self.fake_screen, pg.Color('red'), start2, end2)
-#         
+#
 #         print(end1.angle_to(end2))
 #         for s in self.all_sprites:
 #             pg.draw.rect(self.fake_screen, pg.Color('red'), s.rect, 1)
 # =============================================================================
-        
+
         transformed_screen = pg.transform.scale(self.game_screen,
                                                 self.display_rect.size)
         self.display_screen.blit(transformed_screen, (0, 0))
         pg.display.update()
-        
-        
+
+
     def run(self):
         self.running = True
         while self.running:
             delta_time = self.clock.tick(self.fps) / 1000
             pg.display.set_caption(f'FPS: {round(self.clock.get_fps(), 2)}')
-            self.events()        
+            self.events()
             self.update(delta_time)
             self.draw()
-        
+
         pg.quit()
-        
-        
-        
+
+
+
 class Mode7:
     def __init__(self, game, sprite=None, size=(1024, 1024)):
         self.game = game
@@ -183,41 +183,11 @@ class Mode7:
         
         self.near_x2 = player.pos.x + cos(player.angle + self.fov_half) * self.near
         self.near_y2 = player.pos.y + sin(player.angle + self.fov_half) * self.near
-        
-        
-        # loop over every pixel on the image, beginning furthest away towards the
-        # camera point
-        for y in range(screen_rect.h):
-            # take a sample point for depth linearly related to rows on the screen
-            sample_depth = y / screen_rect.h + 0.0000001 # this prevents div by 0 errors
-            # not sure how this is handled in the c++ code
-            
-            # Use sample point in non-linear (1/x) way to enable perspective
-            # and grab start and end points for lines across the screen
-            start_x = (self.far_x1 - self.near_x1) / sample_depth + self.near_x1
-            start_y = (self.far_y1 - self.near_y1) / sample_depth + self.near_y1
-            end_x = (self.far_x2 - self.near_x2) / sample_depth + self.near_x2
-            end_y = (self.far_y2 - self.near_y2) / sample_depth + self.near_y2
-            
-            # Linearly interpolate lines across the screen
-            for x in range(screen_rect.w):
-                sample_width = x / screen_rect.w
-                sample_x = (end_x - start_x) * sample_width + start_x
-                sample_y = (end_y - start_y) * sample_width + start_y
-                
-                # Wrap sample coordinates to give "infinite" periodicity on maps
-                sample_x = sample_x % 1
-                sample_y = sample_y % 1
-                
-                # sample a color from the image
-                # translate x and y to screen proportions first because they are fractions of 1
-                col = self.image.get_at((int(sample_x * self.rect.w), 
-                                         int(sample_y * self.rect.h)))
-                # set the pixel values of the fake screen image
-                # get_at and set_at are super slow, gonna try pixel arrays instead
-                screen.set_at((x, int(y + screen_rect.h * horizon)),
-                                        col)
-    
+
+        near_rect = (self.near_x1, self.near_y1, self.near_x2, self.near_y2)
+        far_rect = (self.far_x1, self.far_y1, self.far_x2, self.far_y2)
+        self._draw(self.image, self.game.game_screen, near_rect, far_rect)
+
         keys = pg.key.get_pressed()
         # control the rendering parameters
         # this is for debugging only!
@@ -235,7 +205,49 @@ class Mode7:
             self.fov_half -= 0.2 * dt
         elif keys[pg.K_e]:
             self.fov_half += 0.2 * dt
-            
+
+    @staticmethod
+    def _draw(source, screen, near_rect, far_rect):
+        HORIZON = 0.2
+        width, height = source.get_rect().size
+        screen_width, screen_height = screen.get_rect().size
+        near_x1, near_y1, near_x2, near_y2 = near_rect
+        far_x1, far_y1, far_x2, far_y2 = far_rect
+
+        # loop over every pixel on the image, beginning furthest away towards the
+        # camera point
+        get_ = source.get_at
+        set_ = screen.set_at
+        for y in range(screen_height):
+            # take a sample point for depth linearly related to rows on the screen
+            sample_depth = y/screen_height + 0.0000001  # this prevents div by 0 errors
+            # not sure how this is handled in the c++ code
+
+            # Use sample point in non-linear (1/x) way to enable perspective
+            # and grab start and end points for lines across the screen
+            start_x = (far_x1 - near_x1)/sample_depth + near_x1
+            start_y = (far_y1 - near_y1)/sample_depth + near_y1
+            end_x = (far_x2 - near_x2)/sample_depth + near_x2
+            end_y = (far_y2 - near_y2)/sample_depth + near_y2
+
+            # Linearly interpolate lines across the screen
+            for x in range(screen_width):
+                sample_width = x/screen_width
+                sample_x = (end_x - start_x)*sample_width + start_x
+                sample_y = (end_y - start_y)*sample_width + start_y
+
+                # Wrap sample coordinates to give "infinite" periodicity on maps
+                sample_x = sample_x%1
+                sample_y = sample_y%1
+
+                # sample a color from the image
+                # translate x and y to screen proportions first because they are fractions of 1
+                col = get_((int(sample_x*width),
+                            int(sample_y*height)))
+                # set the pixel values of the fake screen image
+                # get_at and set_at are super slow, gonna try pixel arrays instead
+                set_((x, int(y + screen_height*HORIZON)), col)
+
 
 # ENUMS correspond to kart image index
 LEFT = [5, 4, 3, 2, 1, 0]
@@ -251,36 +263,36 @@ class Player(pg.sprite.Sprite):
         self.acc = vec()
         self.vel = vec()
         self.speed = 0.2
-        
+
         self.image = game.player_images[LEFT[0]]
         self.rect = self.image.get_rect()
         self.rect.topleft = (84, 84)
-        
+
         self.time_passed = 0 # seconds from the start of the game
         self.steer_time = 0 # seconds the player is pressing a direction
         self.lastdir = 'LEFT'
         self.moving = 1 # 1 forward, -1 backwards
         self.dust_timer = 0
-        
-    
+
+
     def update(self, dt):
         self.time_passed += dt
 
         keys = pg.key.get_pressed()
-        
+
         if not self.game.started:
             if keys[pg.K_w]:
                 self.dust_timer += dt
                 if self.dust_timer >= 0.3:
                     # create two particles (left and right)
-                    Particle(self.game, self.rect.bottomright, 
+                    Particle(self.game, self.rect.bottomright,
                              images=[self.game.cloud_image],
                              colors=[pg.Color('white')],
                              vel=vec(1, 0),
                              random_angle=20,
                              vanish_speed=20,
                              end_size=1.4)
-                    Particle(self.game, self.rect.bottomleft, 
+                    Particle(self.game, self.rect.bottomleft,
                              images=[self.game.cloud_image],
                              colors=[pg.Color('white')],
                              vel=vec(-1, 0),
@@ -288,20 +300,20 @@ class Player(pg.sprite.Sprite):
                              vanish_speed=20,
                              end_size=1.4)
                     self.dust_timer = 0
-            
+
         else:
             if self.moving == 1:
                 turn_force = 20 # how much the angle changes when turning
             else:
                 turn_force = 50
             steer_anim_speed = 10 # turning animation speed
-            
+
             current_speed = self.vel.length()
-            
+
             # steer
             # cap the image index at 4 (len of animation minus 2)
             index = min(4, int(self.steer_time * steer_anim_speed))
-            
+
             if keys[pg.K_a]:
                 # turning left
                 if self.lastdir == 'RIGHT':
@@ -335,12 +347,12 @@ class Player(pg.sprite.Sprite):
                     self.image = self.game.player_images[RIGHT[index]]
                 elif self.lastdir == 'RIGHT' and self.moving == -1:
                     self.image = self.game.player_images[LEFT[index]]
-                
+
                 self.steer_time -= dt
-            
+
             # limit the steer time between 0 and the maximum image index
             self.steer_time = max(min(self.steer_time, steer_anim_speed * dt), 0)
-                
+
             # move forward or backwards
             if keys[pg.K_w]:
                 self.moving = 1
@@ -350,22 +362,22 @@ class Player(pg.sprite.Sprite):
                 self.moving = -1
                 self.acc.x = self.speed * -0.4
                 self.acc.y = self.speed * -0.4
-    
+
             self.vel.x += self.acc.x * cos(self.angle) * dt
             self.vel.y += self.acc.y * sin(self.angle) * dt
             self.pos += self.vel * dt
-            
+
             self.acc *= 0
             self.vel *= 0.9
-    
-            
+
+
             # move image up and down
             if int(self.time_passed * 5) % 2 == 0 and current_speed >= 0.01:
                 self.rect.top = 85
             else:
                 self.rect.top = 84
-                
-            
+
+
             # create dust clouds
             self.dust_timer += dt
             if self.dust_timer >= 0.2:
@@ -375,7 +387,7 @@ class Player(pg.sprite.Sprite):
                     v = vec(-3, 0) * self.moving
                 if self.steer_time >= 0.3 and current_speed > 0.04:
                     if self.moving == 1:
-                        p = Particle(self.game, self.rect.midbottom, 
+                        p = Particle(self.game, self.rect.midbottom,
                                      images=[self.game.cloud_image],
                                      colors=[pg.Color('white')],
                                      vel=v, random_angle=30,
@@ -383,17 +395,17 @@ class Player(pg.sprite.Sprite):
                                      end_size=1.4)
                         p.add_force(vec(0, 1), 10)
                     else:
-                        p = Particle(self.game, self.rect.midbottom, 
+                        p = Particle(self.game, self.rect.midbottom,
                                      images=[self.game.cloud_image],
                                      colors=[pg.Color('white')],
                                      vel=v, random_angle=30,
                                      vanish_speed=20,
                                      end_size=0.9)
                         p.add_force(vec(0, -2), 10)
-                    
+
                 self.dust_timer = 0
-        
-        
+
+
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
@@ -410,8 +422,8 @@ class TrafficLight(pg.sprite.Sprite):
         self.done = False
         self.timer = 0
         self.img_index = 0
-        
-    
+
+
     def update(self, dt):
         self.timer += dt
         if self.timer >= 1:
@@ -423,12 +435,12 @@ class TrafficLight(pg.sprite.Sprite):
                 self.image = self.images[self.img_index]
             except:
                 self.kill()
-        
+
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-        
-        
+
+
 
 class Bush(pg.sprite.Sprite):
     # TODO: work in progress
@@ -439,8 +451,8 @@ class Bush(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         # position on the map
         self.map_pos = map_pos
-        
-        
+
+
     def update(self, dt):
         # calculate screen position and size based on relative position
         # on game map to the player
